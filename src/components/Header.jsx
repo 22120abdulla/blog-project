fab941afddfef7b4a79eda6a0ebf1870c07938e4
nv22120 <154723822+22120abdulla@users.noreply.github.com>
2024-02-25 18:46:36 +0000

    ttt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 import { images } from "../constants";
 
 const navItemsInfo = [
@@ -23,13 +25,32 @@ return  (
 };
 
 const Header = () => {
+const [navIsVisible, setNavIsVisible] = useState(false);
+
+const navVisibilityHandler = () => {
+  setNavIsVisible((curstate) => {
+    return !curstate;
+  });
+};
+
   return (
     <section>
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
-          <img src={images.Logo} alt="logo" />
+          <img className='w-16' src={images.Logo} alt="logo" />
         </div>
-        <div className='flex gap-x-9 items-center'>
+        <div>
+          {navIsVisible ? (
+          <AiOutlineClose 
+          className="w-6 h-6" 
+          onClick={navVisibilityHandler}
+          /> 
+          ) : (
+          <AiOutlineMenu 
+          className='w-6 h-6' onClick={navVisibilityHandler} />
+        )}
+        </div>
+        <div className= {'${navIsVisible ? "right-0" : "-right-full"} flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row top-0 bootom-0 lg:static gap-x-9 items-center'}>
           <ul className='flex gap-x-2 font-semibold'>
             {navItemsInfo.map((item) => (
             <NavItem key={item.name} name={item.name} />
