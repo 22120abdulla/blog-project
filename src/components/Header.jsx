04f18108629a0c06849a6b31960444fbe8256c57
nv22120 <154723822+22120abdulla@users.noreply.github.com>
2024-02-28 15:30:42 +0000

    `1`

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,34 +4,32 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { images } from "../constants";
 
 const navItemsInfo = [
-  {name: "Home"},
-  {name: "Articles"},
-  {name: "Pages"},
-  {name: "Pricing"},
-  {name: "Faq"},
+  { name: "Home" },
+  { name: "Articles" },
+  { name: "Pages" },
+  { name: "Pricing" },
+  { name: "Faq" },
 ];
 
 const NavItem = ({ name }) => {
-return  (
-  <li className='relative group'>
-    <a href="/" className='px-4 py-2'>
-      {name}
-    </a>
-    <spam className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
-      /
-    </spam>
-</li>
- );
+  return (
+    <li className="relative group">
+      <a href="/" className="px-4 py-2">
+        {name}
+      </a>
+      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+        /
+      </span>
+    </li>
+  );
 };
 
 const Header = () => {
-const [navIsVisible, setNavIsVisible] = useState(false);
+  const [navIsVisible, setNavIsVisible] = useState(false);
 
-const navVisibilityHandler = () => {
-  setNavIsVisible((curstate) => {
-    return !curstate;
-  });
-};
+  const navVisibilityHandler = () => {
+    setNavIsVisible((prevState) => !prevState);
+  };
 
   return (
     <section>
@@ -39,31 +37,36 @@ const navVisibilityHandler = () => {
         <div>
           <img className="w-16" src={images.Logo} alt="logo" />
         </div>
-        <div className='z-50'>
+        <div className="lg:hidden z-50">
           {navIsVisible ? (
-          <AiOutlineClose 
-          className="w-6 h-6" 
-          onClick={navVisibilityHandler} /> 
+            <AiOutlineClose
+              className="w-6 h-6"
+              onClick={navVisibilityHandler}
+            />
           ) : (
-          <AiOutlineMenu 
-          className='w-6 h-6' onClick={navVisibilityHandler} />
-        )}
+            <AiOutlineMenu
+              className="w-6 h-6"
+              onClick={navVisibilityHandler}
+            />
+          )}
         </div>
-        <div 
-        className={`${
+        <div
+          className={`${
             navIsVisible ? "right-0" : "-right-full"
-          } mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
-            >
-          <ul  className="flex flex-col lg:flex-row gap-x-2 font-semibold">
-          {navItemsInfo.map((item) => (
-            <NavItem key={item.name} item={item.name} />
-          ))}
-        </ul>
-          <button className='border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all dura'>Sign in</button>
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+        >
+          <ul className="text-white item-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+            {navItemsInfo.map((item, index) => (
+              <NavItem key={index} name={item.name} />
+            ))}
+          </ul>
+          <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+            Sign in
+          </button>
         </div>
       </header>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
