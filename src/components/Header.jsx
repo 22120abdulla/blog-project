89bc4571285451226a2fa7369abf3edb6732168e
nv22120 <154723822+22120abdulla@users.noreply.github.com>
2024-03-04 15:05:41 +0000

    8 - Footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { MdKeyboardArrowDown } from "react-icons/md";
-
-
 import { images } from "../constants";
 
 const navItemsInfo = [
@@ -66,7 +64,7 @@ const Header = () => {
   };
 
   return (
-    <section className="sticky top-0 left-0 right-0 z-50 bg-whit">
+    <section className="sticky top-0 left-0 right-0 z-50 bg-white">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img className="w-16" src={images.Logo} alt="logo" />
