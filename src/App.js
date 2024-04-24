d28a75f1f4dd60564301a212edc0a13087c6f237
nv22120 <154723822+22120abdulla@users.noreply.github.com>
2024-04-24 05:21:18 +0000

    25 - Profile page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
 
 
 import "./App.css";
@@ -7,6 +7,7 @@ import ArticleDetailPage from "./pages/articleDetail/ArticleDetailpage";
 import HomePage from "./pages/home/HomePage";
 import RegisterPage from "./pages/register/RegisterPage";
 import LoginPage from "./pages/login/LoginPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Toaster />
     </div>
